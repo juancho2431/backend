33f0a4e1a3a747b0b27150038a7591293ee91d7b
juancho2431 <91489626+juancho2431@users.noreply.github.com>
2025-01-17 18:52:18 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('pos_arepasaurios', 'pos_arepasaurios_user', 'UZtNzCitO4a3UFeVxBlTHHp0OiqEceYP', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
   logging: false, // Desactiva los logs de Sequelize para hacer más limpia la salida
+  dialectOptions: {
+    ssl: {
+      require: true, // Obliga a usar SSL
+      rejectUnauthorized: false, // Acepta certificados no firmados
+    },
+  },
   pool: {
     max: 5, // Número máximo de conexiones en el pool
     min: 0, // Número mínimo de conexiones en el pool
     acquire: 30000, // Tiempo máximo en ms para intentar obtener una conexión antes de lanzar un error
-    idle: 10000 // Tiempo máximo que una conexión puede estar inactiva antes de ser liberada
-  }
+    idle: 10000, // Tiempo máximo que una conexión puede estar inactiva antes de ser liberada
+  },
 });
+
 
 // Probar la conexión
 async function connectDB() {
